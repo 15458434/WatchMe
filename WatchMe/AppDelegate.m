145323b7426aa92a17b1eb58fc9b520a954c9417
Mark Cornelisse <m.p.cornelisse@gmail.com>
2020-08-16 20:07:19 +0200

    - Updated the ApplicationDelegate.

--- WatchMe/AppDelegate.m
+++ WatchMe/AppDelegate.m
@@ -8,20 +8,44 @@
 
 #import "AppDelegate.h"
 
-@interface AppDelegate ()
+@interface AppDelegate () <NSMenuItemValidation>
 
 @end
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+- (void)reopenWindow {
+    NSApplication *application = NSApplication.sharedApplication;
+    if (application.windows.count == 0) {
+        NSWindowController *windowController = (NSWindowController *)[[NSStoryboard alloc] instantiateControllerWithIdentifier:@"MainWindow"];
+        [windowController showWindow:self];
+    } else if (application.mainWindow) {
+        [application.mainWindow makeKeyAndOrderFront:self];
+    } else {
+        NSWindow *window = application.windows.firstObject;
+        [window makeKeyAndOrderFront:self];
+    }
 }
 
-
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+- (void)showWindow:(id)sender {
+    [self reopenWindow];
 }
 
+#pragma mark - NSMenuItemValidation
+
+- (BOOL)validateMenuItem:(NSMenuItem *)menuItem {
+    if ([self respondsToSelector:menuItem.action]) {
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
+#pragma mark - NSApplicationDelegate
+
+- (BOOL)applicationShouldHandleReopen:(NSApplication *)sender hasVisibleWindows:(BOOL)flag {
+    [self reopenWindow];
+    return YES;
+}
 
 @end
